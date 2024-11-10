6185b2496fd41f4c609aaf455bcb7c3f5ec6ffbf
Ododah David <95848067+Ododahdavid@users.noreply.github.com>
2024-11-10 15:44:11 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+// importing styles for styling
+import "./styles/styles.css"
+// importing dependencies
+import React, { Suspense } from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUpPage from "./pages/SignUpPage.jsx"
+import LoginPage from "./pages/LogInPage.jsx"
+const DashBoard = React.lazy(()=> import("./pages/DashBoardPage.jsx"))
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+                <Route path="/signUpPage" element={<SignUpPage/>} />
+                <Route path="/logInPage" element={<LoginPage/>} />
+                <Route path="/dashBoard" element={<DashBoard/>}/>
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/DashBoardPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SideBar from '../components/dashboardComponents/SideBar'
+
+const DashBoardPage = () => {
+  return (
+    <>
+        <section className={"dashboardContainer"}>
+            <SideBar/>
+        </section>
+
+    
+    </>
+  )
+}
+
+export default DashBoardPage
